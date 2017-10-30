c488c6cf3a770d75de574561f6aeafaf67b7e446
mshav <mbulelobakana@gmail.com>
2017-10-30 16:37:23 +0200

    adding stock

--- public/shoe.js
+++ public/shoe.js
@@ -3,17 +3,23 @@ $(function() {
 var mysecTemplate = document.querySelector(".mysecTemplate").innerHTML;
 var Table = Handlebars.compile(mysecTemplate);
 var showTable = document.getElementById('showTable')
-var button = document.getElementById('button').innerHTML;
+var brand = document.getElementById('brand')
+var color = document.getElementById('color')
+var size = document.getElementById('size')
+var price = document.getElementById('price')
+var in_stock = document.getElementById('in_stock')
 
       $.ajax({
         type: "GET",
-        url: '/shoe/api',
+        // url: '/shoe/api',
         dataType: 'json',
         success: function(data) {
+          console.log(data);
 
           showTable.innerHTML = Table({
             shoedata: data.data
           })
+
         },
         error: function(){
         alert("error!")
@@ -21,16 +27,39 @@ var button = document.getElementById('button').innerHTML;
 
       })
 
+$('#add').on("click", function(){
+$.ajax({
+type: "POST",
+url: '/api/shoe',
+dataType: 'json',
+data:{
+brand:brand.value,
+color:color.value,
+price:price.value,
+in_stock:in_stock.value,
+size:size.value
+},
+success: function(data){
+brand.innerHTML = Table({
+
+shoedata: data.data
+
+})
+
+
+}
 
 
 
-      $.ajax({
-      type: "POST",
-      url:'/api/shoe',
-      dataType: 'json',
-      succes: function(data) {
+})
 
-      }
 
-      })
+
+
+
+
+
+})
+// window.location.reload(1)
+
 })
